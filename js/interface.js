7eadd4db00da601fb453668eb012db762b8a9ca2
CHEMOAnthony <anthonychemo@icloud.com>
2025-01-11 02:24:18 +0100

    [style]ajout modification

--- js/interface.js
+++ js/interface.js
@@ -1,10 +1,14 @@
+//declaration des variables contenant l'id de chaque boutton sur le menu
 const id1=document.getElementById("idaccueil");
 const id2=document.getElementById("idexercer");
 const id3=document.getElementById("iddefis");
 const id4=document.getElementById("idclassement");
 const id5=document.getElementById("idhistorique");
 const id6=document.getElementById("idaide");
+const id7=document.getElementById("exopred");
 const global=document.getElementById("idglobe");
+const exo=document.getElementById("idexercice");
+
 //Interface section accueil
 id1.addEventListener("click", function(){
 
@@ -12,29 +16,83 @@ id1.addEventListener("click", function(){
 //   const tittle_text=document.createTextNode("BIENVENUE DANS L'ACCUEIL");
 //   tittle.appendChild(tittle_text);
 //   global.appendChild(tittle);
-global.textContent = "BIENVENUE DANS L'ACCUEIL";
+global.innerHTML = `<h1>BIENVENUE DANS L'ACCUEIL</h1>`;
 global.className +=' '+'mef';
 });
+//interface section s'exercer
 id2.addEventListener("click", function(){ 
-    global.textContent = "BIENVENUE DANS S'EXERCER"; 
     global.className +=' '+'mef';
+    global.innerHTML=`<h1>BIENVENUE DANS S'EXERCER</h1>
+    <div class="exo">
+        <div class="image-exo">
+            <img src="../images/illustration1.jpg" alt="exercice predefini">
+        </div>
+        <div class="bloc-exo">
+            <h3 class="titre-exo">Exercice Predefini</h3>
+            <p class="description-exo">Entrainez vous avec des exercices selectionnés avec soin pour vous.</p>
+        </div>
+        <div class="action-exo">
+            <div id="exopred" class="btn-exo">
+                <i class="bi bi-arrow-right"></i>
+            </div>
+            
+        </div>
+    </div>
+       
+    <div class="exo">
+        <div class="image-exo">
+            <img src="../images/illustration2.jpg" alt="exercice personnalise">
+        </div>
+        <div class="bloc-exo">
+            <h3 class="titre-exo">Exercice Personnalisé</h3>
+            <p class="description-exo">Choisissez vos propres textes pour vous entrainer.</p>
+                
+        </div>
+        <div class="action-exo">
+            <div class="btn-exo">
+                <i class="bi bi-arrow-right"></i>
+            </div>
+        </div>
+       
+    </div>
+  </div>`;
 });
-
+//Interface section defis
 id3.addEventListener("click", function(){ 
-    global.textContent = "BIENVENUE DANS DEFIS"; 
+    global.innerHTML= `<h1>BIENVENUE DANS DEFIS</h1>`; 
     global.className +=' '+'mef';
 });
+//Interface section classement
 id4.addEventListener("click", function(){ 
-    global.textContent = "BIENVENUE DANS CLASSEMENT"; 
+    global.innerHTML =`<h1>BIENVENUE DANS CLASSEMENT</h1>`; 
     global.className +=' '+'mef';
 });
+//Interface section historique
 id5.addEventListener("click", function(){ 
-    global.textContent = "BIENVENUE DANS HISTORIQUE"; 
+    global.innerHTML =`<h1>BIENVENUE DANS HISTORIQUE</h1>`; 
     global.className +=' '+'mef';
  });
+ //Interface section aide
  id6.addEventListener("click", function(){ 
-    global.textContent = "BIENVENUE DANS AIDE"; 
-    global.className +=' '+'mef';
+    global.innerHTML =`<h1 class="mef">BIENVENUE DANS AIDE</h1>
+    <div class="aide">
+    <h2 class="conseil">CONSEIL</h2>
+    <br>
+    <P>Pour ameliorer votre vitesse de saisie vous pouvez:</P>
+    <ul>
+      <ol>1-Pour ciommencer, il est esssentiel de maintenir une bonne posture devant l'ecran, <br>
+          en gardant le dos droit et les bras à la bonne hauteur;</ol>
+      <ol>2-Adoptez une position correcte de la main et les doigts entre chaque frappe;</ol>
+      <ol>3-Utiliser vos mains pour accelerer la saisie;</ol>
+      <ol>4-Maitriser toutes les touches du clavier; </ol>
+      <ol>5-Eviter de trop regarder le clavier;</ol>
+    </ul>
+  </div>`; 
+    //global.className +=' '+'';
+ });
+ id7.addEventListener("click", function(){ 
+    global.textContent = "EXERCICE PREDEFINIS"; 
+    global.className +=' '+'mefe';
  });
 
    
